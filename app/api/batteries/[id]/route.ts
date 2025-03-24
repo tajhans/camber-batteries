8bded5b27f0ab58890c1798c48a345ecc97ed263
Taj Hans <83455454+tajhans@users.noreply.github.com>
2025-03-24 08:50:58 -0500

    Update route.ts

--- app/api/batteries/[id]/route.ts
+++ app/api/batteries/[id]/route.ts
@@ -4,10 +4,10 @@ import { batteryFormSchema } from "@/lib/schema";
 
 export async function GET(
     request: NextRequest,
-    { params }: { params: { id: string } },
+    { params }: { params: Record<string, string | string[]> }
 ) {
     try {
-        const id = params.id;
+        const id = Array.isArray(params.id) ? params.id[0] : params.id;
         const battery = await redis.hgetall(`battery:${id}`);
 
         if (!battery || !Object.keys(battery).length) {
@@ -35,10 +35,10 @@ export async function GET(
 
 export async function PATCH(
     request: NextRequest,
-    { params }: { params: { id: string } },
+    { params }: { params: Record<string, string | string[]> }
 ) {
     try {
-        const id = params.id;
+        const id = Array.isArray(params.id) ? params.id[0] : params.id;
         const body = await request.json();
 
         const result = batteryFormSchema.safeParse(body);
